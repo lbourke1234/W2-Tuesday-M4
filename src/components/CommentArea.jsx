79740037b3c8f964ac9b66c68ac40c119dc288a3
lbourke1234 <lbourke1234@gmail.com>
2022-04-26 15:54:25 +0100

    update

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -18,29 +18,30 @@ class CommentArea extends Component {
     }
 
     componentDidUpdate = async (prevProps, prevState) => {
+        if (prevProps.asin !== this.props.asin) {
 
-        try {
-            let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
-                headers: {
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTA5ODI3MDAsImV4cCI6MTY1MjE5MjMwMH0.9aZzmvN4RzBfRGqquXRDzYFutHPIzC_DR3lo7SmsR_w'
+            try {
+                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
+                    headers: {
+                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTA5ODI3MDAsImV4cCI6MTY1MjE5MjMwMH0.9aZzmvN4RzBfRGqquXRDzYFutHPIzC_DR3lo7SmsR_w'
+                    }
+                })
+                // console.log(response)
+                
+                if (response.ok) {
+                    let comments = await response.json()
+                    console.log(comments)
+                    this.setState({ comments: comments, isLoading: false, isError: false })
+                } else {
+                    console.log('error')
+                    this.setState({ isLoading: false, isError: true })
                 }
-            })
-            // console.log(response)
-            
-            if (response.ok) {
-                let comments = await response.json()
-                console.log(comments)
-                this.setState({ comments: comments, isLoading: false, isError: false })
-            } else {
-                console.log('error')
+            } catch (error) {
+                console.log(error)
                 this.setState({ isLoading: false, isError: true })
             }
-        } catch (error) {
-            console.log(error)
-            this.setState({ isLoading: false, isError: true })
-        }
-    }
-
+        }}
+        
     render() {
         return (
             <div>
